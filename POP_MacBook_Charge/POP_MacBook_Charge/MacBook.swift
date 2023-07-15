8d9e69685b75271b4e3b0a5cf201131926844a40
Whales <Whales.w.Jin@gmail.com>
2023-07-15 10:14:22 +0900

    feat: chargeBattery 메서드 구현

--- POP_MacBook_Charge/POP_MacBook_Charge/MacBook.swift
+++ POP_MacBook_Charge/POP_MacBook_Charge/MacBook.swift
@@ -8,10 +8,12 @@
 // 맥북은 각각 다 다르니까 값 타입.
 struct MacBook {
     let permittedChargeWattPerHour: WattPerHour
-    var currentBattery: WattPerHour
-    let maximumBattery: WattPerHour = 100
+    var currentBattery: Watt
+    let maximumBattery: Watt = 100
     
     func chargeBattery(charger: Chargeable) {
         // 충전기를 활용해 배터리를 완전히 충전한 뒤 충전에 걸린 시간을 print
+        let chargeTime = (maximumBattery - currentBattery) / charger.maximumWattPerHour
+        print(chargeTime)
     }
 }
